7c1b79f0c216abb8bad9a09d18ba370d9732abd3
Fran Varney <fran@modulus.io>
2015-10-25 22:15:05 -0400

    Add cookbook routes

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const Child = require('child_process');
 
+const Cookbook = require('./controllers/cookbook');
 const Package = require('../package.json');
 const User = require('./controllers/user');
 
@@ -27,5 +28,12 @@ module.exports = [
   { method: 'GET', path: '/user/{username}', handler: User.find },
   { method: 'POST', path: '/user/create', handler: User.create, config: { auth: false } },
   { method: 'PUT', path: '/user/{username}', handler: User.update },
-  { method: 'DELETE', path: '/user/{username}', handler: User.delete }
+  { method: 'DELETE', path: '/user/{username}', handler: User.delete },
+
+  // Cookbook routes
+  { method: 'GET', path: '/user/{username}/cookbooks', handler: Cookbook.allByUser },
+  { method: 'GET', path: '/cookbook/{id}', handler: Cookbook.find },
+  { method: 'POST', path: '/cookbook/create', handler: Cookbook.create },
+  { method: 'PUT', path: '/cookbook/{id}', handler: Cookbook.update },
+  { method: 'DELETE', path: '/cookbook/{id}', handler: Cookbook.delete }
 ];
