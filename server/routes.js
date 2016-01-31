30e151d87bcec832fc810555d1bb561ad91e4fab
CodeCaffe <codecaffe@gmail.com>
2016-01-30 20:28:32 -0500

    + DB Routes for testing

--- server/routes.js
+++ server/routes.js
@@ -20,5 +20,28 @@ module.exports = [
   { method: 'GET', path: '/cookbook/{id}', handler: Cookbook.find },
   { method: 'POST', path: '/cookbook/create', handler: Cookbook.create },
   { method: 'PUT', path: '/cookbook/{id}', handler: Cookbook.update },
-  { method: 'DELETE', path: '/cookbook/{id}', handler: Cookbook.delete }
+  { method: 'DELETE', path: '/cookbook/{id}', handler: Cookbook.delete },
+
+  { method:  'GET', path: '/admin/users/migrate', handler:function(req, res){
+    const Manager = require('knex-schema');
+    const DB = require('./connections/postgres');
+    const manager = new Manager(DB);
+    const User = require('../database/schemas/users.js');
+    manager.sync([User]);
+  }},
+
+  { method:  'GET', path: '/admin/users/seed', handler:function(req, res){
+    const Manager = require('knex-schema');
+    const DB = require('./connections/postgres');
+    const manager = new Manager(DB);
+    const User = require('../database/schemas/users.js');
+    manager.populate([User]);
+  }},
+  { method:  'GET', path: '/admin/users/drop', handler:function(req, res){
+    const Manager = require('knex-schema');
+    const DB = require('./connections/postgres');
+    const manager = new Manager(DB);
+    const User = require('../database/schemas/users.js');
+    manager.drop([User]);
+  }}
 ];
