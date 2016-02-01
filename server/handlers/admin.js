be293cd5d14ea9eab5b11b29fe36f4c241035d0b
Fran Varney <fran@modulus.io>
2016-01-31 23:09:32 -0500

    Seperate out admin routes

---
+++ server/handlers/admin.js
@@ -0,0 +1,29 @@
+const Manager = require('knex-schema');
+
+const Cookbooks = require('../../database/schemas/cookbooks');
+const DB = require('../connections/postgres');
+const Recipes = require('../../database/schemas/recipes');
+const Units = require('../../database/schemas/units');
+const Users = require('../../database/schemas/users');
+
+const manager = new Manager(DB);
+
+exports.migrat = function (request, reply) {
+  manager.sync([Units, Users, Recipes, Cookbooks]);
+  reply('Move bitch, get out the way!');
+};
+
+exports.reset = function (request, reply) {
+  manager.reset([Cookbooks, Recipes, Units, Users]);
+  reply('Resetedededed');
+};
+
+exports.seed = function (request, reply) {
+  manager.seed([Units, Users, Recipes, Cookbooks]);
+  reply('Put your seeds in me.');
+};
+
+exports.drop = function (request, reply) {
+  manager.drop([Units, Users, Recipes, Cookbooks]);
+  reply('Drop it like it\'s hot.');
+};
---
+++ server/routes/index.js
@@ -0,0 +1,29 @@
+const Admin = require('../handlers/admin');
+const Cookbook = require('../handlers/cookbook');
+const Package = require('../../package.json');
+const Ping = require('../handlers/ping');
+const User = require('../handlers/user');
+
+module.exports = [
+  // Test route
+  { method: 'GET', path: '/ping', handler: Ping, config: { auth: false } },
+
+  // Admin routes
+  { method: 'GET', path: '/admin/migrate', handler: Admin.migrate },
+  { method: 'GET', path: '/admin/seed', handler: Admin.reset },
+  { method: 'GET', path: '/admin/seed', handler: Admin.seed },
+  { method: 'GET', path: '/admin/drop', handler: Admin.drop },
+
+  // Cookbook routes
+  { method: 'GET', path: '/user/{username}/cookbooks', handler: Cookbook.allByUser },
+  { method: 'GET', path: '/cookbook/{id}', handler: Cookbook.find },
+  { method: 'POST', path: '/cookbook/create', handler: Cookbook.create },
+  { method: 'PUT', path: '/cookbook/{id}', handler: Cookbook.update },
+  { method: 'DELETE', path: '/cookbook/{id}', handler: Cookbook.delete },
+
+  // User routes
+  { method: 'GET', path: '/user/{username}', handler: User.find },
+  { method: 'POST', path: '/user/create', handler: User.create, config: { auth: false } },
+  { method: 'PUT', path: '/user/{username}', handler: User.update },
+  { method: 'DELETE', path: '/user/{username}', handler: User.delete }
+];
