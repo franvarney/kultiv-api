9154bfd00c4b61d728d440147d3a03b4db5a44e9
Fran Varney <fran@modulus.io>
2016-01-31 19:42:27 -0500

    Add recipes_directions schema

---
+++ database/schemas/recipes-directions.js
@@ -0,0 +1,31 @@
+const DB = require('../../server/connections/postgres');
+
+const RecipesDirections = {
+  tableName: 'recipes_directions',
+  timestamps: true,
+  timestampFn: DB.fn.now(),
+  build: function (table) {
+    table.increments('id').primary();
+    table.integer('recipe_id').unsigned().references('id').inTable('recipes');
+    table.integer('direction_id').unsigned().references('id').inTable('directions');
+    table.timestamp('deleted_at').nullable();
+  },
+  populate: function (database) {
+    return database('recipes_directions').insert([
+      {
+        recipe_id: 1,
+        direction_id: 1
+      },
+      {
+        recipe_id: 2,
+        direction_id: 2
+      },
+      {
+        recipe_id: 3,
+        direction_id: 3
+      }
+    ]);
+  }
+};
+
+module.exports = RecipesDirections;
