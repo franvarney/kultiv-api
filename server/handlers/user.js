ba6ad9ff593956f7ca5e87d336676bbcc1a52f09
Fran Varney <fran@modulus.io>
2016-02-01 22:26:52 -0500

    Redo user.find handler

--- server/handlers/user.js
+++ server/handlers/user.js
@@ -2,146 +2,134 @@ const Boom = require('boom');
 // const Debug = require('debug')('cookbook/src/controllers/user');
 const Waterfall = require('run-waterfall');
 
-exports.create = function (request, reply) {
-  const User = request.server.plugins['hapi-mongo-models'].User;
+const User = require('../models/user');
 
-  Waterfall([
-    function (callback) {
-      User.isExisting(
-        request.payload.email,
-        request.payload.username,
-        function (err, isExisting) {
-          if (err) return callback(err);
-          if (isExisting) {
-            return callback({ message: 'User already exists' });
-          }
-          callback();
-        });
-    },
-    User.hashPassword.bind(null, request.payload.password),
-    function (hashedPassword, callback) {
-      User.validate({
-        email: request.payload.email,
-        username: request.payload.username,
-        password: hashedPassword
-      }, callback);
-    },
-    function (validatedUser, callback) {
-      User.insertOne(validatedUser, callback);
-    }
-  ], function (err, user) {
-    if (err) return reply(Boom.badRequest(err.message));
-    reply(user[0].username);
-  });
-};
+// exports.create = function (request, reply) {
+//   const User = request.server.plugins['hapi-mongo-models'].User;
 
-exports.delete = function (request, reply) {
-  const User = request.server.plugins['hapi-mongo-models'].User;
+//   Waterfall([
+//     function (callback) {
+//       User.isExisting(
+//         request.payload.email,
+//         request.payload.username,
+//         function (err, isExisting) {
+//           if (err) return callback(err);
+//           if (isExisting) {
+//             return callback({ message: 'User already exists' });
+//           }
+//           callback();
+//         });
+//     },
+//     User.hashPassword.bind(null, request.payload.password),
+//     function (hashedPassword, callback) {
+//       User.validate({
+//         email: request.payload.email,
+//         username: request.payload.username,
+//         password: hashedPassword
+//       }, callback);
+//     },
+//     function (validatedUser, callback) {
+//       User.insertOne(validatedUser, callback);
+//     }
+//   ], function (err, user) {
+//     if (err) return reply(Boom.badRequest(err.message));
+//     reply(user[0].username);
+//   });
+// };
 
-  Waterfall([
-    User.isExisting.bind(null, request.params.username),
-    function (user, callback) {
-      if (!user) return callback({ message: 'User not found' });
-      User.deleteOne(
-        { username: request.params.username },
-        function (err) {
-        if (err) return callback(err);
-        callback();
-      });
-    }
-  ], function (err) {
-    if (err) return reply(Boom.notFound(err.message));
-    reply({ success: true });
-  });
-};
+// exports.delete = function (request, reply) {
+//   const User = request.server.plugins['hapi-mongo-models'].User;
+
+//   Waterfall([
+//     User.isExisting.bind(null, request.params.username),
+//     function (user, callback) {
+//       if (!user) return callback({ message: 'User not found' });
+//       User.deleteOne(
+//         { username: request.params.username },
+//         function (err) {
+//         if (err) return callback(err);
+//         callback();
+//       });
+//     }
+//   ], function (err) {
+//     if (err) return reply(Boom.notFound(err.message));
+//     reply({ success: true });
+//   });
+// };
 
 exports.find = function (request, reply) {
-  const User = request.server.plugins['hapi-mongo-models'].User;
-
-  Waterfall([
-    User.isExisting.bind(null, request.params.username),
-    function (user, callback) {
-      if (!user) return callback({ message: 'User not found' });
-      User.findOne(
-        { username: request.params.username },
-        function (err, foundUser) {
-        if (err) return callback(err);
-        callback(null, foundUser);
-      });
-    }
-  ], function (err, foundUser) {
-    if (err) return reply(Boom.notFound(err.message));
-    delete foundUser.password;
-    // delete foundUser.authToken; // TODO figure out if this should be returned
-    reply(foundUser);
+  User.findById(request.params.id, (err, user) => {
+    if (err) return reply(Boom.badRequest(err));
+    if (!user) return reply(Boom.notFound('User not found'));
+    return reply(user).code(200);
   });
 };
 
-exports.update = function (request, reply) {
-  var user;
-  const User = request.server.plugins['hapi-mongo-models'].User;
+// exports.update = function (request, reply) {
+//   var user;
+//   const User = request.server.plugins['hapi-mongo-models'].User;
 
-  Waterfall([
-    User.isExisting.bind(
-      null,
-      request.payload.email,
-      request.params.username
-    ),
-    function (existingUser, callback) {
-      if (!existingUser) {
-        return callback({ message: 'User not found' });
-      }
-      user = existingUser;
-      callback(null, existingUser);
-    },
-    function (existingUser, callback) {
-      if (request.payload.password) {
-        User.isPasswordMatch(
-          request.payload.password,
-          existingUser.password,
-          function (err, isMatch) {
-            if (err) return callback(err);
-            callback(null, isMatch);
-          });
-      } else {
-        callback(null, true);
-      }
-    },
-    function (isMatch, callback) {
-      if (!isMatch) {
-        User.hashPassword(
-          request.payload.password,
-          function (err, hashedPassword) {
-          if (err) return callback(err);
-          callback(null, hashedPassword);
-        });
-      } else {
-        callback(null, false);
-      }
-    },
-    function (hashedPassword, callback) {
-      user.email = request.payload.email;
-      user.location = request.payload.location;
-      user.updated = Date.now();
+//   Waterfall([
+//     User.isExisting.bind(
+//       null,
+//       request.payload.email,
+//       request.params.username
+//     ),
+//     function (existingUser, callback) {
+//       if (!existingUser) {
+//         return callback({ message: 'User not found' });
+//       }
+//       user = existingUser;
+//       callback(null, existingUser);
+//     },
+//     function (existingUser, callback) {
+//       if (request.payload.password) {
+//         User.isPasswordMatch(
+//           request.payload.password,
+//           existingUser.password,
+//           function (err, isMatch) {
+//             if (err) return callback(err);
+//             callback(null, isMatch);
+//           });
+//       } else {
+//         callback(null, true);
+//       }
+//     },
+//     function (isMatch, callback) {
+//       if (!isMatch) {
+//         User.hashPassword(
+//           request.payload.password,
+//           function (err, hashedPassword) {
+//           if (err) return callback(err);
+//           callback(null, hashedPassword);
+//         });
+//       } else {
+//         callback(null, false);
+//       }
+//     },
+//     function (hashedPassword, callback) {
+//       user.email = request.payload.email;
+//       user.location = request.payload.location;
+//       user.updated = Date.now();
 
-      if (hashedPassword) user.password = hashedPassword;
+//       if (hashedPassword) user.password = hashedPassword;
 
-      User.validate(user, function (err, validatedUser) {
-        if (err) return callback(err);
-        callback(null, validatedUser);
-      });
-    },
-    function (validatedUser, callback) {
-      User.updateOne(
-        { username: request.params.username },
-        validatedUser,
-        function (err) {
-        if (err) return callback(err);
-        callback();
-      });
-    }
-  ], function (err) {
-    if (err) return reply(Boom.badRequest(err.message));
-    reply(request.params.username);
-  });
-};
+//       User.validate(user, function (err, validatedUser) {
+//         if (err) return callback(err);
+//         callback(null, validatedUser);
+//       });
+//     },
+//     function (validatedUser, callback) {
+//       User.updateOne(
+//         { username: request.params.username },
+//         validatedUser,
+//         function (err) {
+//         if (err) return callback(err);
+//         callback();
+//       });
+//     }
+//   ], function (err) {
+//     if (err) return reply(Boom.badRequest(err.message));
+//     reply(request.params.username);
+//   });
+// };
--- server/models/user.js
+++ server/models/user.js
@@ -1,111 +1,13 @@
-var User;
+const Knex = require('../connections/postgres');
 
-const BaseModel = require('hapi-mongo-models').BaseModel;
-const Bcrypt = require('bcrypt');
-// const Debug = require('debug')('cookbook/src/model/user');
-const Joi = require('joi');
-const ObjectAssign = require('object-assign');
-const Parallel = require('run-parallel');
-const ShortId = require('shortid');
-const ShortId32 = require('shortid32');
-
-const SALT_WORK_FACTOR = 10;
-ShortId32.characters('23456789abcdefghjklmnpqrstuvwxyz');
-
-User = BaseModel.extend({
-  constructor: function (attrs) {
-    ObjectAssign(this, attrs);
-  }
-});
-
-User._collection = 'users'; // eslint-disable-line
-
-function toLower(string) {
-  return string.toLowerCase();
-}
-
-User.schema = Joi.object().keys({
-  _id: Joi.string().default(ShortId32.generate, 'Generate short id 32'),
-  email: Joi.string().required().email().trim().default(toLower, 'Make lower case'),
-  username: Joi.string().required().alphanum().min(2).max(20).trim().default(toLower, 'Make lower case'),
-  password: Joi.string().required().regex(/[a-zA-Z0-9]{3,30}/),
-  location: Joi.string().optional().allow('').trim(),
-  isAdmin: Joi.boolean().default(false),
-  authToken: Joi.string().default(ShortId.generate, 'Generate short id'),
-  resetPassword: Joi.object().keys({
-    token: Joi.string().required(),
-    expires: Joi.date().required()
-  }),
-  created: Joi.date().raw().default(Date.now, 'Registered'),
-  updated: Joi.date().raw(),
-  deleted: Joi.date().raw()
-});
-
-User.indexes = [
-  [{ email: 1 }, { unique: true }],
-  [{ username: 1 }, { unique: true }],
-  [{ authToken: 1 }, { unique: false }],
-  [{ created: 1}, { unique: false }]
-];
-
-User.findByCredentials = function (username, password, done) {
-  var query = {};
-
-  if (username.indexOf('@') > -1) {
-    query.email = username.toLowerCase();
-  } else {
-    query.username = username.toLowerCase();
-  }
-
-  User.findOne(query, done);
+exports.findById = function (id, done) {
+  Knex('users').where({
+    id: id,
+    deleted_at: null
+  })
+  .then((user) => {
+    if (user.length < 1) return done(null, false);
+    return done(null, user[0]);
+  })
+  .catch((err) => done(err));
 };
-
-User.findByEmail = function (email, done) {
-  var query = { email: email.toLowerCase() };
-  User.findOne(query, done);
-};
-
-User.findByToken = function (token, done) {
-  var query = { authToken: token };
-  User.findOne(query, done);
-};
-
-User.findByUsername = function (username, done) {
-  var query = { username: username.toLowerCase() };
-  User.findOne(query, done);
-};
-
-User.hashPassword = function (password, done) {
-  Bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
-    if (err) return done(err);
-
-    Bcrypt.hash(password, salt, function (err, hashedPassword) {
-      if (err) return done(err);
-      done(null, hashedPassword);
-    });
-  });
-};
-
-User.isExisting = function (email, username, done) {
-  var tasks = [];
-  if (email.indexOf('@') === -1) {
-    done = username;
-    username = email;
-    email = null;
-  }
-
-  if (email) tasks.push(User.findByEmail.bind(this, email));
-  tasks.push(User.findByUsername.bind(this, username));
-
-  Parallel(tasks, function (err, results) {
-    if (err) return done(err);
-    if (results[0] || results[1]) return done(null, results[0] || results[1]);
-    done(null, false);
-  });
-};
-
-User.isPasswordMatch = function (password, hashedPassword, done) {
-  Bcrypt.compare(password, hashedPassword, done);
-};
-
-module.exports = User;
