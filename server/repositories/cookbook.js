56cda90914fccfc07dc7a1a614e2b34254182849
Fran Varney <fran@modulus.io>
2016-02-05 09:33:09 -0500

    server/repositories/cookbook.js

--- server/repositories/cookbook.js
+++ server/repositories/cookbook.js
@@ -1,46 +1,68 @@
 'use strict';
 
+const Base = require('./base');
 const CookbookModel = require('../models/cookbook');
 const DB = require('../connections/postgres');
-const Base = require('base.js');
+const User = require('./user');
+
+const TABLE_NAME = 'cookbooks';
 
 class Cookbook extends Base {
-
   constructor() {
-    super('cookbooks', CookbookModel);
+    super(TABLE_NAME, CookbookModel);
   }
 
-  //GET
-  findByOwner(ownerUserId){
-    //DB->where id == userId
+  findByOwner(ownerUserId, done) {
+    this.db
+      .where('owner_id', ownerUserId)
+      .then((cookbooks) => done(null, cookbooks))
+      .catch((err) => done(err));
   }
 
-  findByCollaborator(collaboratorUserId){
-   DB("cookbook").innerJoin("cookbooks-collaborators as cc", "id", "cc.id")
-           .where("cc.user_id", collaboratorUserId)
-           .then(function(err,cookbooks){
-              //
-            })
-           .catch(function(err){
-              console.log(err);
-            });
+  findByCollaborator(collaboratorUserId, done) {
+    this.db
+      .innerJoin('cookbooks-collaborators as CC', 'id', 'CC.collaborator_id')
+      .where('CC.collaborator_id', collaboratorUserId)
+      .then((cookbooks) => done(null, cookbooks))
+      .catch((err) => done(err));
   }
 
-  //POST
-  create(cookbook){
-    //DB->insert.....
+  create(payload, done) {
+    User.findById(payload.user_id, (err, user) => {
+      if (err) return done(err);
+      if (!user) return done(err, false);
+      this.add(validated, done);
+    });
   }
 
-  //PUT
-  update(id, cookbook){
-    //DB->update.....
+  update(id, payload, done) {
+    this.validate(payload, this.schema, (err, validated) => {
+      if (err) return done(err);
+
+      this.db
+        .where('id', id)
+        .update(payload)
+        .returning('id')
+        .then((id) => done(null, id))
+        .catch((err) => done(err));
+    });
   }
 
-  //PATCH
-  toggleIsPrivate(id){
-    //DB->isPrivate = !isPrivate
-  }
+  toggleIsPrivate(id) {
+    this.findById(id, (err, cookbook) => {
+      if (err) return done(err);
 
+      var isPrivate = true;
+      if (cookbook.is_private) isPrivate = false;
+
+      this.db
+        .where('id', id)
+        .update('is_private', isPrivate)
+        .returning('id')
+        .then((id) => done(null, id))
+        .catch((err) => done(err));
+    });
+  }
 }
 
 module.exports = new Cookbook();
