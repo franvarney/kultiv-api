9f189216d0cf1575ed6b889046ebec73906ce1b3
CodeCaffe <codecaffe@gmail.com>
2016-02-06 23:09:28 -0500

    + Adjusted for treeize

--- server/repositories/recipe.js
+++ server/repositories/recipe.js
@@ -31,16 +31,16 @@ class Recipe extends Base {
 
     var baseRecipe = function (queryBuilder) {
       queryBuilder
-        .select('recipes.id', 'recipes.title', 'recipes.cook_time',
-                'recipes.prep_time', 'recipes.description', 'recipes.is_private',
-                'recipes.created_at', 'recipes.updated_at',
+        .select('recipes.id AS id', 'recipes.title AS details:title', 'recipes.cook_time AS details:cook_time',
+                'recipes.prep_time AS details:prep_time', 'recipes.description AS details:description', 'recipes.is_private AS details:is_private',
+                'recipes.created_at AS details:created_at', 'recipes.updated_at AS details:updated_at',
                 'recipes.user_id AS user:id', 'U.username AS user:username',
                 'recipes.yield_amount AS yield:amount', 'RU.name AS yield:unit')
         .innerJoin('users AS U', 'U.id', 'recipes.user_id')
         .innerJoin('units AS RU', 'RU.id', 'recipes.yield_unit_id')
         .whereNull('recipes.deleted_at')
         .groupBy('recipes.id', 'U.username', 'RU.name')
-    }
+    };
 
     var ingredients = function (queryBuilder) {
       queryBuilder
@@ -51,7 +51,7 @@ class Recipe extends Base {
             .innerJoin('foods AS F', 'I.food_id', 'F.id')
             .innerJoin('units AS IU', 'I.unit_id', 'IU.id')
         .groupBy('RI.recipe_id', 'I.id', 'IU.name', 'F.name')
-    }
+    };
 
     var directions = function (queryBuilder) {
       queryBuilder
@@ -59,7 +59,7 @@ class Recipe extends Base {
         .innerJoin('recipes_directions AS RD', 'recipes.id', 'RD.recipe_id')
           .innerJoin('directions AS D', 'RD.direction_id', 'D.id')
         .groupBy('D.id')
-    }
+    };
 
     if (!isLoaded) {
       this.db
