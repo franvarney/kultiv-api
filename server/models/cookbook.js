15517c1fb3def2518bd6e3dec2cedcc327e228be
CodeCaffe <codecaffe@gmail.com>
2016-02-12 22:43:44 -0500

    + Corrected Cookbook Model to allow nulls

--- server/models/cookbook.js
+++ server/models/cookbook.js
@@ -5,10 +5,10 @@ const Joi = require('joi');
 const CookbookModel = Joi.object().keys({
   owner_id: Joi.number().integer().required(),
   name: Joi.string().min(3).max(50),
-  description: Joi.string(),
+  description: Joi.string().allow(null),
   is_private: Joi.boolean(),
   updated_at: Joi.string().default('now()').allow(null),
-  deleted_at: Joi.string().allow('now()')
+  deleted_at: Joi.string().allow(null)
 });
 
 module.exports = CookbookModel;
--- server/routes/index.js
+++ server/routes/index.js
@@ -7,32 +7,32 @@ const User = require('../handlers/user');
 
 module.exports = [
   // Test route
-  {method: 'GET', path: '/ping', handler: Ping, config: {auth: false}},
+  { method: 'GET', path: '/ping', handler: Ping, config: { auth: false } },
 
   // Admin routes
-  {method: 'GET', path: '/admin/migrate', handler: Admin.migrate},
-  {method: 'GET', path: '/admin/reset', handler: Admin.reset},
-  {method: 'GET', path: '/admin/seed', handler: Admin.seed},
-  {method: 'GET', path: '/admin/drop', handler: Admin.drop},
-  {method: 'GET', path: '/admin/users', handler: Admin.users},
+  { method: 'GET', path: '/admin/migrate', handler: Admin.migrate },
+  { method: 'GET', path: '/admin/reset', handler: Admin.reset },
+  { method: 'GET', path: '/admin/seed', handler: Admin.seed },
+  { method: 'GET', path: '/admin/drop', handler: Admin.drop },
+  { method: 'GET', path: '/admin/users', handler: Admin.users },
 
   // Cookbook routes
-  {method: 'GET', path: '/users/{user_id}/cookbooks', handler: Cookbook.allByUser},
-  {method: 'GET', path: '/cookbooks/{id}', handler: Cookbook.get},
-  {method: 'POST', path: '/cookbooks', handler: Cookbook.create},
-  // { method: 'PUT', path: '/cookbooks/{id}', handler: Cookbook.update },
-  {method: 'DELETE', path: '/cookbooks/{id}', handler: Cookbook.delete},
+  { method: 'GET', path: '/users/{user_id}/cookbooks', handler: Cookbook.allByUser },
+  { method: 'GET', path: '/cookbooks/{id}', handler: Cookbook.get },
+  { method: 'POST', path: '/cookbooks', handler: Cookbook.create },
+  { method: 'PUT', path: '/cookbooks/{id}', handler: Cookbook.update },
+  { method: 'DELETE', path: '/cookbooks/{id}', handler: Cookbook.delete },
 
   // Recipe routes
-  {method: 'GET', path: '/users/{id}/recipes', handler: Recipe.allByUser},
-  {method: 'GET', path: '/recipes/{id}', handler: Recipe.get},
-  {method: 'POST', path: '/recipes', handler: Recipe.create},
-  // { method: 'PUT', path: '/recipes/{id}', handler: Recipe.update },
-  {method: 'DELETE', path: '/recipes/{id}', handler: Recipe.delete},
+  { method: 'GET', path: '/users/{id}/recipes', handler: Recipe.allByUser },
+  { method: 'GET', path: '/recipes/{id}', handler: Recipe.get },
+  { method: 'POST', path: '/recipes', handler: Recipe.create },
+  { method: 'PUT', path: '/recipes/{id}', handler: Recipe.update },
+  { method: 'DELETE', path: '/recipes/{id}', handler: Recipe.delete },
 
   // User routes
-  {method: 'GET', path: '/users/{id}', handler: User.get},
-  {method: 'POST', path: '/users', handler: User.create, config: {auth: false}},
-  {method: 'PUT', path: '/users/{id}', handler: User.update},
-  {method: 'DELETE', path: '/users/{id}', handler: User.delete}
+  { method: 'GET', path: '/users/{id}', handler: User.get },
+  { method: 'POST', path: '/users', handler: User.create, config: { auth: false } },
+  { method: 'PUT', path: '/users/{id}', handler: User.update },
+  { method: 'DELETE', path: '/users/{id}', handler: User.delete }
 ];
