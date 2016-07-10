c714aa01bd1932331f8afd146f2453ee8eec3794
Fran Varney <fvarney@progress.com>
2016-07-10 13:24:15 -0400

    Remove plugin (nevermind that)

--- server/plugins/index.js
+++ server/plugins/index.js
@@ -1,10 +1,8 @@
 const HapiHawk = require('hapi-auth-hawk')
 
 const HapiTreeize = require('./hapi-treeize')
-const Reformat = require('./reformat')
 
 module.exports = [
   HapiHawk,
-  HapiTreeize,
-  Reformat
+  HapiTreeize
 ]
