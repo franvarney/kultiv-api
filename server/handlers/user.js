da49ce484a03cbb80301de3994a0ac7d44a36f43
Fran Varney <fvarney@progress.com>
2016-07-10 17:41:48 -0400

    Add error handler

--- server/handlers/user.js
+++ server/handlers/user.js
@@ -1,13 +1,13 @@
-const {badRequest} = require('boom')
 const Logger = require('franston')('server:handlers:user')
 
+const Errors = require('../utils/errors')
 const User = require('../models/user')
 
 exports.create = function (request, reply) {
   Logger.debug('user.create')
 
   User.create(request.payload, (err, id) => {
-    if (err) return Logger.error(err), reply(badRequest(err))
+    if (err) return Logger.error(err), reply(Errors.get(err))
     return Logger.debug(id), reply(id).code(201)
   })
 }
@@ -16,7 +16,7 @@ exports.delete = function (request, reply) {
   Logger.debug('user.delete')
 
   User.deleteById(request.params.id, (err) => {
-    if (err) return Logger.error(err), reply(badRequest(err))
+    if (err) return Logger.error(err), reply(Errors.get(err))
     return reply().code(204)
   })
 }
@@ -25,7 +25,7 @@ exports.get = function (request, reply) {
   Logger.debug('user.get')
 
   User.findById(request.params.id, (err, user) => {
-    if (err) return Logger.error(err), reply(badRequest(err))
+    if (err) return Logger.error(err), reply(Errors.get(err))
     return Logger.debug(user), reply(user).code(200)
   })
 }
@@ -34,7 +34,7 @@ exports.update = function (request, reply) {
   Logger.debug('user.update')
 
   User.update(request.params.id, request.payload, (err, id) => {
-    if (err) return Logger.error(err), reply(badRequest(err))
+    if (err) return Logger.error(err), reply(Errors.get(err))
     return Logger.debug(id), reply(id).code(200)
   })
 }
