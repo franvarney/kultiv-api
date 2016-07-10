c87053733b3a3cc7668ddee87256dde95827a841
Fran Varney <fvarney@progress.com>
2016-07-10 18:40:31 -0400

    Use error handler

--- server/handlers/cookbook.js
+++ server/handlers/cookbook.js
@@ -1,13 +1,15 @@
-const {badRequest} = require('boom')
 const Logger = require('franston')('server:handlers:cookbook')
 
-const Cookbook = require('../models/cookbook')
+const CookbookModel = require('../models/cookbook')
+const Errors = require('../utils/errors')
+
+const Cookbook = new CookbookModel
 
 exports.allByUser = function (request, reply) {
   Logger.debug('cookbook.allByUser')
 
   Cookbook.findByOwner(request.params.user_id, (err, cookbooks) => {
-    if (err) return Logger.error(err), reply(badRequest(err))
+    if (err) return Logger.error(err), reply(Errors.get(err))
     return Logger.debug(cookbooks), reply.treeize(cookbooks).code(200)
   })
 }
@@ -16,7 +18,7 @@ exports.create = function (request, reply) {
   Logger.debug('cookbook.create')
 
   Cookbook.create(request.payload, (err, id) => {
-    if (err) return Logger.error(err), reply(badRequest(err))
+    if (err) return Logger.error(err), reply(Errors.get(err))
     return Logger.debug(id), reply({ id }).code(201)
   })
 }
@@ -25,7 +27,7 @@ exports.delete = function (request, reply) {
   Logger.debug('cookbook.delete')
 
   Cookbook.deleteById(request.params.id, (err) => {
-    if (err) return Logger.error(err), reply(badRequest(err))
+    if (err) return Logger.error(err), reply(Errors.get(err))
     return reply().code(204)
   })
 }
@@ -34,8 +36,8 @@ exports.get = function (request, reply) {
   Logger.debug('cookbook.get')
 
   Cookbook.findById(request.params.id, (err, cookbook) => {
-    if (err) return Logger.error(err), reply(badRequest(err))
-    return Logger.debug(cookbook), reply(cookbook).code(200)
+    if (err) return Logger.error(err), reply(Errors.get(err))
+    return reply().code(204)
   })
 }
 
