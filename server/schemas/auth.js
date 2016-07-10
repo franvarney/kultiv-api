91ed0f6261bdf8fdb9414a15ab96688c821725ad
Fran Varney <fvarney@progress.com>
2016-07-10 13:30:16 -0400

    Remove unused schema

--- server/schemas/auth.js
+++ server/schemas/auth.js
@@ -9,12 +9,3 @@ exports.loginPayload = Joi.object({
   login: Joi.alternatives().try(Joi.string(), Joi.string().email()).required(),
   password: Joi.string().required()
 })
-
-exports.loginResponse = Joi.object({
-  id: Joi.string().guid().required(),
-  key: Joi.string().guid().required(),
-  userId: Joi.number().integer()
-})
-  .rename('hawk_id', 'id')
-  .rename('hawk_key', 'key')
-  .rename('user_id', 'userId')
