0bc16732d0c72316b7935167d2b79cffa1506387
Fran Varney <fvarney@progress.com>
2016-07-17 20:29:45 -0400

    Add lodash util

---
+++ server/utils/lodash.js
@@ -0,0 +1,9 @@
+const IsEqual = require('lodash.isequal')
+const Pick = require('lodash.pick')
+const XorWith = require('lodash.xorwith')
+
+module.exports = {
+  isEqual: IsEqual,
+  pick: Pick,
+  xorWith: XorWith
+}
