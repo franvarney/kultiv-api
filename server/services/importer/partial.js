de000687bfb09840cc587d3d3ef31848dc1d8855
Fran Varney <fvarney@progress.com>
2016-07-18 00:22:18 -0400

    Add partial recipe importer with tests

---
+++ server/services/importer/partial.js
@@ -0,0 +1,107 @@
+function split(text) {
+  return text.split(/\n/g)
+}
+
+function decimal(numerator, denominator) {
+  return numerator / denominator
+}
+
+function parseIngredients(ingredients, done) {
+  if (!ingredients) {
+    return done('No Ingredients')
+  }
+
+  ingredients = split(ingredients)
+
+  let newIngredients = []
+  ingredients.some((ingredient) => {
+    if (!ingredient) return
+
+    let create = new Array(4)
+
+    // check if optional first. if so, remove the optional string
+    let index = ingredient.indexOf('optional')
+    if (index > -1) {
+      create[3] = true
+      ingredient = ingredient.slice(0, index - 2) // -2 removes ', ' then 'optional'
+    } else create[3] = false
+
+    // split string on space
+    ingredient = ingredient.split(' ')
+
+    // check if index 0 is a number or 'a' which would mean '1'. if so, set
+    // index 0 equal to the number
+    if (ingredient[0].indexOf('/') > -1) {
+      let [numerator, denominator] = ingredient.shift().split('/')
+      create[0] = decimal(numerator, denominator)
+    } else if (!isNaN(Number(ingredient[0]))) {
+      create[0] = Number(ingredient.shift())
+    } else if (ingredient[0].toLowerCase() === 'a' ||
+               ingredient[0].toLowerCase() === 'an') {
+      ingredient.shift()
+      create[0] = 1
+    } else create[0] = null
+
+    // if there's not two values left, that probably means there is no unit
+    if (!ingredient[1]) {
+      create[1] = null
+      create[2] = ingredient.shift()
+    } else {
+      // check for fractions in index 1
+      if (ingredient[0].indexOf('/') > -1) {
+        let [numerator, denominator] = ingredient.shift().split('/')
+        create[0] = create[0] + decimal(numerator, denominator)
+      }
+
+      create[1] = ingredient.shift()
+      create[2] = ingredient.join(' ')
+
+      // if the food starts with of, remove it
+      if (create[2].indexOf('of') === 0) {
+        create[2] = create[2].substring(3)
+      }
+
+      // if the food starts with and, then add index 1 to index 2 and reset index 1
+      if (create[2].indexOf('and') === 0) {
+        create[2] = `${create[1]} ${create[2]}`
+        create[1] = null
+       }
+    }
+
+    newIngredients.push({
+      amount: create[0],
+      unit: create[1],
+      food: create[2],
+      optional: create[3]
+    })
+  })
+
+  return done(null, newIngredients)
+}
+
+function parseDirections(directions, done) {
+  if (!directions) {
+    return done('No Directions')
+  }
+
+  directions = split(directions)
+  directions = directions.filter((direction) => !!direction)
+  directions = directions.map((direction) => ({ direction }))
+
+  return done(null, directions)
+}
+
+module.exports = function (payload, done) {
+  let {ingredients, directions} = payload
+
+  parseIngredients(ingredients, (err, parsed) => {
+    if (err) return done(err)
+    payload.ingredients = parsed
+
+    parseDirections(directions, (err, parsed) => {
+      if (err) return done(err)
+      payload.directions = parsed
+      return done(null, payload)
+    })
+  })
+}
---
+++ test/services/importer/partial.js
@@ -0,0 +1,214 @@
+const Code = require('code')
+const Lab = require('lab')
+
+const lab = exports.lab = Lab.script()
+const {describe, it} = lab
+const {expect} = Code
+
+const PartialImporter = require('../../../server/services/importer/partial')
+
+let ingredients = [
+  '1 cup peppers, sliced',
+  '1 1/2 tomato, optional',
+  'salt and pepper, to taste',
+  'handful of lettuce',
+  'cilantro',
+  ''
+].join('\n')
+
+let directions = [
+  'direction 1',
+  'direction 2',
+  'direction 3',
+  ''
+].join('\n')
+
+describe('services/importer/partial', () => {
+  describe('partially entered recipe', () => {
+    it('yields a recipe with array of ingredients and directions', (done) => {
+      PartialImporter({ recipe_id: 1, ingredients, directions }, (err, recipe) => {
+        expect(err).to.be.null()
+        expect(Array.isArray(recipe.ingredients)).to.be.true()
+        expect(Array.isArray(recipe.directions)).to.be.true()
+        return done()
+      })
+    })
+  })
+
+  describe('parses ingredients', () => {
+    describe('when no ingredients', () => {
+      it('yield an error', (done) => {
+        let ingredients = ''
+        let directions = ['Direction 1', 'Direction 2']
+
+        PartialImporter({ ingredients, directions }, (err) => {
+          expect(err).to.equal('No Ingredients')
+          return done()
+        })
+      })
+    })
+
+    describe('when amount includes integers and fractions', () => {
+      it('combines numbers', (done) => {
+        let ingredients = ['1 1/2 cups apples'].join('\n')
+
+        PartialImporter({ ingredients, directions }, (err, recipe) => {
+          expect(err).to.be.null()
+          expect(recipe.ingredients[0].amount).to.equal(1.5)
+          expect(recipe.ingredients[0].unit).to.equal('cups')
+          return done()
+        })
+      })
+    })
+
+    describe('when amount includes fractions', () => {
+      it('combines numbers', (done) => {
+        let ingredients = ['1/2 cups apples'].join('\n')
+
+        PartialImporter({ ingredients, directions }, (err, recipe) => {
+          expect(err).to.be.null()
+          expect(recipe.ingredients[0].amount).to.equal(0.5)
+          expect(recipe.ingredients[0].unit).to.equal('cups')
+          return done()
+        })
+      })
+    })
+
+    describe('when amount includes "a" or "an"', () => {
+      it('removes "a" or "an"' , (done) => {
+        let ingredients = ['an apple', 'a pepper'].join('\n')
+
+        PartialImporter({ ingredients, directions }, (err, recipe) => {
+          expect(err).to.be.null()
+          expect(recipe.ingredients[0].amount).to.equal(1)
+          expect(recipe.ingredients[1].amount).to.equal(1)
+          return done()
+        })
+      })
+    })
+
+    describe('when no amount', () => {
+      it('sets amount property to null', (done) => {
+        let ingredients = ['handful lettuce'].join('\n')
+
+        PartialImporter({ ingredients, directions }, (err, recipe) => {
+          expect(err).to.be.null()
+          expect(recipe.ingredients[0].amount).to.be.null()
+          expect(recipe.ingredients[0].unit).to.equal('handful')
+          return done()
+        })
+      })
+    })
+
+    describe('when no unit', () => {
+      it('sets the unit property to null', (done) => {
+        let ingredients = ['1/2 apple'].join('\n')
+
+        PartialImporter({ ingredients, directions }, (err, recipe) => {
+          expect(err).to.be.null()
+          expect(recipe.ingredients[0].unit).to.equal(null)
+          expect(recipe.ingredients[0].food).to.equal('apple')
+          return done()
+        })
+      })
+    })
+
+    describe('when optional', () => {
+      it('sets the optional property to true', (done) => {
+        let ingredients = [
+          '1/2 apple, optional',
+          '1/2 apple (optional)'
+        ].join('\n')
+
+        PartialImporter({ ingredients, directions }, (err, recipe) => {
+          expect(err).to.be.null()
+          expect(recipe.ingredients[0].optional).to.be.true()
+          expect(recipe.ingredients[1].optional).to.be.true()
+          return done()
+        })
+      })
+    })
+
+    describe('when no amount or unit', () => {
+      it('sets amount and unit properties to null', (done) => {
+        let ingredients = ['salt'].join('\n')
+
+        PartialImporter({ ingredients, directions }, (err, recipe) => {
+          expect(err).to.be.null()
+          expect(recipe.ingredients[0].amount).to.be.null()
+          expect(recipe.ingredients[0].unit).to.be.null()
+          expect(recipe.ingredients[0].food).to.equal('salt')
+          return done()
+        })
+      })
+    })
+
+    describe('when "of" is the start of the food', () => {
+      it('removes "of"', (done) => {
+        let ingredients = ['handful of Panko bread crumbs'].join('\n')
+
+        PartialImporter({ ingredients, directions }, (err, recipe) => {
+          expect(err).to.be.null()
+          expect(recipe.ingredients[0].amount).to.be.null()
+          expect(recipe.ingredients[0].unit).to.equal('handful')
+          expect(recipe.ingredients[0].food).to.equal('Panko bread crumbs')
+          return done()
+        })
+      })
+    })
+
+    describe('when "and" is the start of the food', () => {
+      it('removes "of"', (done) => {
+        let ingredients = ['salt and pepper'].join('\n')
+
+        PartialImporter({ ingredients, directions }, (err, recipe) => {
+          expect(err).to.be.null()
+          expect(recipe.ingredients[0].amount).to.be.null()
+          expect(recipe.ingredients[0].unit).to.be.null()
+          expect(recipe.ingredients[0].food).to.equal('salt and pepper')
+          return done()
+        })
+      })
+    })
+
+    describe('formats the ingredients', () => {
+      it('has amount, unit, food, and optional properties', (done) => {
+        let ingredients = ['1 cup peppers, sliced, optional'].join('\n')
+
+        PartialImporter({ ingredients, directions }, (err, recipe) => {
+          expect(err).to.be.null()
+          expect(recipe.ingredients[0].amount).to.equal(1)
+          expect(recipe.ingredients[0].unit).to.equal('cup')
+          expect(recipe.ingredients[0].food).to.equal('peppers, sliced')
+          expect(recipe.ingredients[0].optional).to.be.true()
+          return done()
+        })
+      })
+    })
+  })
+
+  describe('parses directions', () => {
+    describe('when no directions', () => {
+      it('yield an error', (done) => {
+        let directions = ''
+
+        PartialImporter({ ingredients, directions }, (err) => {
+          expect(err).to.equal('No Directions')
+          return done()
+        })
+      })
+    })
+
+    describe('formats the directions', () => {
+      it('has direction property', (done) => {
+        let directions = ['Direction 1'].join('\n')
+
+        PartialImporter({ ingredients, directions }, (err, recipe) => {
+          expect(err).to.be.null()
+          expect(recipe.directions[0].direction).to.equal('Direction 1')
+          return done()
+        })
+      })
+    })
+  })
+})
