0636310c61ae19255dcaed858515b1670ae36753
Fran Varney <fvarney@progress.com>
2016-07-25 00:35:10 -0400

    Add some recipe tests

--- test/handlers/recipe.js
+++ test/handlers/recipe.js
@@ -1,12 +1,126 @@
-const Code = require('code')
+const {expect} = require('code')
+const {Server} = require('hapi')
 const Lab = require('lab')
 
 const lab = exports.lab = Lab.script()
-const {describe, it} = lab
-const {expect} = Code
+const {after, before, describe, it} = lab
 
-const Auth = require('../../server/handlers/recipe')
+const Recipe = require('../../server/handlers/recipe')
+const RecipeModel = require('../../server/models/recipe')
+
+const server = new Server()
+
+let id
+let unique = Date.now()
 
 describe('handlers/recipe', () => {
-  // TODO
+  describe('create', () => {
+    before((done) => {
+      server.connection()
+      server.route({ method: 'POST', path: '/recipes', handler: Recipe.create })
+      return done()
+    })
+
+    describe('when successfully creates a recipe', () => {
+      it('yields the id', (done) => {
+        server.inject({
+          method: 'POST',
+          url: '/recipes',
+          payload:   {
+            title: `The Best Pizza ${unique}`,
+            cookbook_id: 3,
+            cook_time: 4323,
+            prep_time: 1232,
+            description: 'The best pizza evar!1',
+            is_private: false,
+            directions: [{
+              direction: 'Some directions 1.',
+              order: 1
+            }, {
+              direction: 'Some directions 2.',
+              order: 2
+            }],
+            ingredients: [{
+              amount: 3.2,
+              unit: 'cups',
+              food: 'flour',
+              optional: false
+            }, {
+              amount: 5.6,
+              unit: 'tablespoons',
+              food: 'tomato sauce',
+              optional: false
+            }, {
+              amount: 40,
+              unit: 'slices',
+              food: 'pepperoni',
+              optional: true
+            }],
+            yield_amount: 8,
+            yield_unit: 'servings'
+          },
+          credentials: { user: 3 }
+        }, (response) => {
+          id = response.result.id
+          expect(response.statusCode).to.equal(201)
+          expect(Object.keys(response.result)).to.equal(['id'])
+          expect(typeof response.result.id).to.equal('number')
+          return done()
+        })
+      })
+    })
+  })
+
+  describe('get', () => {
+    before((done) => {
+      server.connection()
+      server.route({ method: 'GET', path: '/recipes/{id}', handler: Recipe.get })
+      return done()
+    })
+
+    describe('when successfully gets a recipe', () => {
+      it('yields the recipe', (done) => {
+        server.inject({
+          method: 'GET',
+          url: `/recipes/${id}`,
+          credentials: { user: 3 }
+        }, (response) => {
+          expect(response.statusCode).to.equal(200)
+          expect(response.result.description).to.equal('The best pizza evar!1')
+          return done()
+        })
+      })
+    })
+
+    // TODO full recipe
+  })
+
+  describe('delete', () => {
+    before((done) => {
+      server.connection()
+      server.route({ method: 'DELETE', path: '/recipes/{id}', handler: Recipe.delete })
+      return done()
+    })
+
+    describe('when successfully deletes a recipe', () => {
+      it('yields no content', (done) => {
+        server.inject({
+          method: 'DELETE',
+          url: `/recipes/${id}`,
+          credentials: { user: 3 }
+        }, (response) => {
+          expect(response.statusCode).to.equal(204)
+          expect(response.result).to.be.null()
+
+          let recipe = new RecipeModel({ payload: { id } })
+
+          return recipe.findById((err, found) => {
+            expect(err[1]).to.equal('Recipe Not Found')
+            expect(found).to.be.undefined()
+            return done()
+          })
+        })
+      })
+    })
+  })
 })
