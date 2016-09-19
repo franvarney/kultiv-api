cfcb8d2c25d9106addfc28ed0fe7fa330cc0c314
Fran Varney <fran.varn@gmail.com>
2016-09-19 11:40:05 -0400

    Update recipe ingredient model

--- server/models/recipe-ingredient.js
+++ server/models/recipe-ingredient.js
@@ -1,28 +1,25 @@
 const Logger = require('franston')('server:models:recipe-ingredient')
 
-const Base = require('./base')
+const Model = require('./base')
 const DB = require('../connections/postgres')
 const Lodash = require('../utils/lodash')
 const RecipeIngredientSchema = require('../schemas/recipe-ingredient')
 
 const TABLE_NAME = 'recipes_ingredients'
 
-class RecipeIngredient extends Base {
-  constructor (data) {
-    super(TABLE_NAME, RecipeIngredientSchema.general, data)
-  }
+const CookbookRecipe = Model.createModel({
+  name: TABLE_NAME,
+  schema: RecipeIngredientSchema.general,
 
   batchFindOrCreate(done) {
     Logger.debug('recipe-ingredient.batchFindOrCreate')
 
-    let that = this
+    let {data} = this
 
     this.knex(this.name)
       .select('id', 'recipe_id', 'ingredient_id')
       .where(function () {
-        that.payload.forEach((recipeIngredient) => {
-          return this.orWhere(recipeIngredient)
-        })
+        data.map((recipeIngredient) => this.orWhere(recipeIngredient))
       })
       .transacting(this.trx)
       .asCallback((err, found) => {
@@ -33,7 +30,7 @@ class RecipeIngredient extends Base {
 
         let ids = []
         let create = Lodash.xorWith(
-          that.payload,
+          data,
           found.map((recipeIngredient) => {
             ids.push(Lodash.pick(found, 'id'))
             return {
@@ -49,23 +46,12 @@ class RecipeIngredient extends Base {
           return done(null, found)
         }
 
-        // TODO validate?
-
-        DB.batchInsert(this.name, create)
-          .returning('id')
-          .transacting(this.trx)
-          .then((created) => {
-            if (this.trx && this.willCommit) {
-              Logger.debug('Transaction Completed'), this.trx.commit()
-            }
-            return done(null, ids.concat(created))
-          })
-          .catch((err) => {
-            if (this.trx) Logger.error('Transaction Failed'), this.trx.rollback()
-            return Logger.error(err), done(err)
-          })
+        this.set(create)._create((err, created) => {
+          if (err) return this._errors(err, done)
+          return done(null, created.concat(ids))
+        })
       })
   }
-}
+})
 
 module.exports = RecipeIngredient
