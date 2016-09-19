004d21205594043ae30737489e5d051ab445e689
Fran Varney <fran.varn@gmail.com>
2016-09-19 09:21:13 -0400

    Update auth check to add scopes

--- server/handlers/auth.js
+++ server/handlers/auth.js
@@ -2,18 +2,25 @@ const Logger = require('franston')('server:handlers:auth')
 
 const Auth = require('../models/auth')
 const Errors = require('../utils/errors')
+const User = require('../models/user')
 
 const ALGORITHM = 'sha256'
 
 exports.getCredentialsFunc = function (id, callback) {
   Logger.debug('auth.getCredentialsFunc')
 
-  // TODO admin token
-
   Auth.set({ id }).findById((err, auth) => {
     if (err) return Logger.error(err), callback(Errors.get(err))
     if (auth) auth.algorithm = ALGORITHM
-    return callback(null, auth)
+
+    User.set({ id: auth.user }).findById((err, user) => {
+      if (err) return Logger.error(err), callback(Errors.get(err))
+
+      auth.scope = auth.scope || ['user']
+      if (user.is_admin === true) auth.scope.push('admin')
+
+      return callback(null, auth)
+    })
   })
 }
 
